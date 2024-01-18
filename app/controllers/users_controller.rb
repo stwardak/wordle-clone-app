7c5e319ccbbe063bf1f9700b4351981f6a70fba3
Cristian B <cris.bahena77@gmail.com>
2024-01-18 12:50:21 -0600

    finished users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,36 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+    render :index
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+    render :show
+  end
+
+  def create
+    @user = User.create(
+      username: params[:username],
+      email: params[:email],
+      password_digest: params[:password_digest]
+    )
+    render :show
+  end
+
+  def updated
+    @user = User.find_by(id: params[:id])
+    @user.update(
+    username: params[:username] || @user.username,
+    email: params[:email] || @user.email,
+    password_digest: params[:password_digest] || @user.password_digest
+    )
+    render :show
+  end
+
+  def destroy
+    @user = User.find_by(id: params[:id])
+    @user.destroy
+    render json: {message: "User succesfully deleted"}
+  end
 end
