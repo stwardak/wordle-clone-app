db8f9271c3d2448426730b0c29c6a9b6ed6e96c3
Cristian B <cris.bahena77@gmail.com>
2024-01-18 13:05:13 -0600

    added words controller data

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,2 +1,14 @@
 class WordsController < ApplicationController
+  def show
+    @word = Word.find_by(id: params[:id])
+    render :show
+  end
+
+  def create
+    @word = Word.create(
+      word: params[:word],
+      difficulty_level: params[:difficulty_level],
+    )
+    render :show
+  end
 end
